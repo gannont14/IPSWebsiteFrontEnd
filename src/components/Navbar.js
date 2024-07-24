cab3c251ebad972944cda4cc2d102b53249eb95b
gtubbs1414 <tubbsgannon14@gmail.com>
2024-07-23 20:20:33 -0400

    updated home page, as well as changing site entirely to light theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import IPSLogo from "../static/IPSLogo.png";
 
-const navbar = ({ className = "bg-gray-800 shadow shadow-black" }) => {
+const navbar = ({
+  navClassName = "bg-white shadow shadow-lg",
+  textClassName = "text-black hover:underline hover:bg-transparent hover:border-black hover:text-black",
+}) => {
   let headers = ["Home", "Services", "About", "Photo Gallery"];
   let headerLinks = ["/", "/services", "/about", "/photos"];
   // mx-8 my-auto  border border-green-500
 
   return (
     <div
-      className={`w-full h-[8rem] flex pl-[30px] transition-all duration-300 fixed ${className}  z-50`}
+      className={`w-full h-[8rem] flex pl-[30px] transition-all duration-300 fixed ${navClassName}  z-50`}
     >
       <img
         className="h-[6rem] rounded-md px-auto py-auto my-auto "
@@ -20,7 +23,7 @@ const navbar = ({ className = "bg-gray-800 shadow shadow-black" }) => {
       <div className="flex">
         {headers.map((header, index) => (
           <Link
-            className="mx-8 my-auto btn btn-outline border-transparent hover:underline hover:bg-transparent hover:border-white hover:text-white text-white "
+            className={`mx-8 my-auto text-lg  btn btn-outline border-transparent ${textClassName}`}
             to={headerLinks[index]}
             key={index}
           >
