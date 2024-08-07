7c17a10f63149f1b2c4373facde5c555a2f3d716
Julian Payne <109392113+jul1anpa@users.noreply.github.com>
2024-08-07 12:05:27 -0400

    Tweaked tailwind config for screen size, added navbar dropdown, etc.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import IPSLogo from "../static/IPSLogo.png";
 
-const navbar = ({
-  navClassName = "bg-white shadow shadow-lg",
+const Navbar = ({
+  navClassName = "bg-white shadow-lg",
   textClassName = "text-black hover:bg-transparent hover:border-black hover:text-black",
 }) => {
-  let headers = ["Home", "Services", "About", "Photo Gallery"];
-  let headerLinks = ["/", "/services", "/about", "/photos"];
+  const headers = ["Home", "Services", "About", "Photo Gallery"];
+  const headerLinks = ["/", "/services", "/about", "/photos"];
+  const location = useLocation();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen((prevState) => !prevState);
+  }
+
+  const getCurrentPage = () => {
+    const currentPath = location.pathname;
+    const index = headerLinks.indexOf(currentPath);
+    return index !== -1 ? headers[index] : "Home";
+  }
   // mx-8 my-auto  border border-green-500
 
   return (
-    <div
-      className={`w-full h-[8rem] flex pl-[30px] transition-all duration-300 fixed ${navClassName}  z-50`}
-    >
-      <button>
-        <Link to="/">
-          <img
-            className="h-[6rem] rounded-md px-auto py-auto my-auto "
-            src={IPSLogo}
-          ></img>
-        </Link>
-      </button>
+    <div className={`w-[100%] h-auto flex items-center px-4 md:px-8 transition-all duration-300 fixed ${navClassName} z-50`}>
+      <div className="hidden md:mt-4 md:flex items-center">
+        <button className="md:mb-0">
+          <Link to="/">
+            <img className="h-24 rounded-md" src={IPSLogo} />
+          </Link>
+        </button>
+      </div>
 
-      {/* left side content */}
-      <div className="flex">
+      {/* Regular navigation */}
+      <div className="hidden md:mt-4 md:flex md:ml-auto border border-primary">
         {headers.map((header, index) => (
           <Link
-            className={`mx-8 my-auto text-lg  btn btn-outline border-transparent ${textClassName}`}
+            className={`mx-4 my-2 md:my-0 text-lg btn btn-outline border-transparent ${textClassName}`}
             to={headerLinks[index]}
             key={index}
           >
@@ -35,13 +44,42 @@ const navbar = ({
           </Link>
         ))}
       </div>
-      {/* right side content */}
-      <div className="flex items-center ml-auto">
-        <button className="mr-[6rem] btn bg-primary text-white font-bold outline outline-primary border-0 hover:bg-white hover:text-black">
+
+      {/* Dropdown navigation for smaller screens */}
+      <div className="flex flex-col mt-8 mx-auto md:hidden border border-primary">
+        <button
+          onClick={toggleDropdown}
+          className={`flex items-center text-lg btn btn-outline border-transparent ${textClassName}`}
+        >
+          {getCurrentPage()}
+          <span className="ml-2">▼</span>
+        </button>
+
+        {dropdownOpen && (
+          <div className="w-full bg-white shadow-lg rounded-md mt-2">
+            {headers.map((header, index) => (
+              <Link
+                to={headerLinks[index]}
+                key={index}
+                className={`block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ${location.pathname === headerLinks[index] ? "bg-gray-200" : ""
+                  }`}
+                onClick={() => setDropdownOpen(false)}
+              >
+                {header}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {/* Contact Us button */}
+      <div className="hidden md:mt-4 md:flex md:ml-auto">
+        <button className="btn bg-primary text-white font-bold outline outline-primary border-0 hover:bg-white hover:text-black">
           <Link to={"/contactus"}>Contact Us</Link>
         </button>
       </div>
     </div>
   );
 };
-export default navbar;
+
+export default Navbar;
